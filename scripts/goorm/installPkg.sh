36fde571997e3a983100bfc15e35ea2135312517
gihyeon.lee <gihyeon.lee@goorm.io>
2020-07-26 07:01:43 +0000

    fix(script): update install package

--- scripts/goorm/installPkg.sh
+++ scripts/goorm/installPkg.sh
@@ -1,3 +1,3 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install -y yarn
+sudo apt update && sudo apt install -y yarn pkg-config libsecret-1-dev libx11-dev libxkbfile-dev
